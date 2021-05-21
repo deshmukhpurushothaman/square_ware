23aa43e6039d1f008ea5ba65d552f8cdde953119
Deshmukh P <deshmukhpurushothaman@gmail.com>
2021-05-21 12:09:47 +0530

    final commit

--- controllers/post.js
+++ controllers/post.js
@@ -16,8 +16,6 @@ const bucketName = process.env.AWS_BUCKET_NAME;
     const accessKeyId = process.env.AWS_ACCESS_KEY;
     const secretAccessKey = process.env.AWS_SECRET_KEY;
 
-    console.log("Bucket Name ", bucketName)
-
 
     const s3 = new S3({
         region,
@@ -27,20 +25,15 @@ const bucketName = process.env.AWS_BUCKET_NAME;
 
 
 exports.profile = async(req,res) => {
-    console.log("Save Image in S3")
     const file = req.file;
-    console.log(file)
-    
-
     const result = await this.uploadFile(file)
-    console.log("AWS S3 ", result)
     await unlinkFile(file.path)
     let post = new Post();
     post.name = req.body.name;
     post.dob = req.body.dob;
     post.bio = req.body.bio;
     post.image = result.Location;
-    console.log("Post ", post)
+    
 
     post.save((err, result) => {
                 if(err){
